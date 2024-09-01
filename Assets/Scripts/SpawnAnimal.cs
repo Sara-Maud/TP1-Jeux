bf01304b2d0ff32e802c29a1170e47dbc3158e95
Sara-Maud <2212088@cegeplimoilou.ca>
2024-09-01 15:00:53 -0400

    Animal Spawn

--- Assets/Scripts/SpawnAnimal.cs
+++ Assets/Scripts/SpawnAnimal.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class SpawnAnimal : MonoBehaviour
 {
     public GameObject[] objectPrefabs;
+
+    private Vector3 spawnPos = new Vector3(0, 0, 25);
+
+    private float repeatDelay = 2f;
+    private float nextDelay = 2f;
+    private float progress;
     // Start is called before the first frame update
     void Start()
     {
-        
+        progress = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        progress += Time.deltaTime;
+
+        //Si le délai est atteint
+        if (progress >= nextDelay)
+        {
+            //Spawner un obstacle et reset le progrès
+            progress = 0f;
+            SpawnObstacle();
+
+            //Prochain délai est aléatoire autour de repeatDelay
+            nextDelay = Random.Range(0.85f * repeatDelay, 1.15f * repeatDelay);
+        }
+
+    }
+
+    void SpawnObstacle()
+    {
+        int index = Random.Range(0, objectPrefabs.Length);
+        repeatDelay *= 0.99f;
+        Instantiate(objectPrefabs[index], spawnPos, objectPrefabs[index].transform.rotation);
     }
 }
