3d03098246cb9b5bdae5123a6557e77ef5141b00
Sara-Maud <2212088@cegeplimoilou.ca>
2024-09-02 15:18:35 -0400

    Ajout rigidbody sur object

--- Assets/Scripts/MoveFood.cs
+++ Assets/Scripts/MoveFood.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class MoveFood : MonoBehaviour
 {
-    private float speed = 2f;
+    private Rigidbody foodRB;
+    private float speed = 4f;
     // Start is called before the first frame update
     void Start()
     {
-
+        foodRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+           transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.gameObject.CompareTag("Animal")) {
+            Destroy(gameObject);
+        //}
     }
 }
