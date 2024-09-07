9eb3fd26c7763dfe1b5a70df43c147e97169fefd
Sara-Maud <2212088@cegeplimoilou.ca>
2024-09-07 16:09:28 -0400

    nourriture se détruit quand elle est loin

--- Assets/Scripts/MoveFood.cs
+++ Assets/Scripts/MoveFood.cs
@@ -6,6 +6,7 @@ public class MoveFood : MonoBehaviour
 {
     private Rigidbody foodRB;
     private float speed = 4f;
+    private float p = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,12 @@ public class MoveFood : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-           transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if(transform.position.z > p)
+        {
+            Destroy(gameObject);
+        }
     }
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Animal")) {
