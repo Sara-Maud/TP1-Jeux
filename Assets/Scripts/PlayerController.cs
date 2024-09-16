70259375351ab598b6e3950b75921dd30d445e6d
Janelle <2264502@cegeplimoilou.ca>
2024-09-16 16:07:20 -0400

    volume et tir

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,7 +53,6 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerAnim.SetTrigger("Tir_Trig");
             particulesNourriture.Play();
             int projectileIndex = Random.Range(0, projectilesPrefabs.Length);
             spawnNourriturePos = new Vector3(transform.position.x + 1, transform.position.y + 2, transform.position.z);
